2193fd24e47e642496897307cce293fd07f3cf49
enerqi <enerqi@enerqi>
2016-08-30 22:33:21 +0100

    Initial skeleton.

---
+++ docs/content/tutorial.fsx
@@ -0,0 +1,19 @@
+(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use 
+// it to define helpers that you do not want to show in the documentation.
+#I "../../bin"
+
+(**
+Introducing your project
+========================
+
+Say more
+
+*)
+#r "Graphs.dll"
+open Graphs
+
+Library.hello 0
+(**
+Some more info
+*)
