5757f4a458e85b191c148bb4df6b00ea7307bcaa
enerqi <enerqi@enerqi>
2016-08-31 14:54:27 +0100

    readGraph WIP

--- src/Graphs/Library.fs
+++ src/Graphs/Library.fs
@@ -1,5 +1,8 @@
 namespace Graphs
 
+open System
+open System.IO
+
 /// Documentation for my library
 ///
 /// ## Example
@@ -7,10 +10,84 @@ namespace Graphs
 ///     let h = Library.hello 1
 ///     printfn "%d" h
 ///
-module Library = 
+module Graphs = 
+
+  type VertexId = VId of int  
+
+  type Vertex<'a> = {
+    //Standard F# types (discriminated unions, records, lists, arrays and tuples) have structural equality semantics. This means that they are 
+    //compared by comparing the actual value stored in them and not by comparing references (even if they are reference types)
+    //All standard F# types are reference types. You can define value type by adding [<Struct>] to your object type declaration, 
+    //but discriminated unions will always be reference types
+    Id: VertexId
+    Neighbours: Vertex<'a> list
+    Weight: 'a option 
+  }  
+
+  type Graph<'a> = {
+    VerticesCount: int
+    EdgesCount: int
+    Vertices: Vertex<'a> array
+  }
   
-  /// Returns 42
+  /// Parse a file and returns a Graph
+  /// File format:
+  ///
+  /// Line 1: n (#vertices) m (# edges)
+  ///         vertices use 1 based indices, 1 to n
+  /// Next m lines = edge u v with id (>= 1 and <= n) - directed or undirected according to the problem.
+  ///              | edge u v w - includes the weight 
+  /// Graph should be simple - no self loops nor parallel edges.
   ///
   /// ## Parameters
-  ///  - `num` - whatever
-  let hello num = 42
+  ///  - `file` - full (relative or absolute) string path to the file to open
+  let readGraph file = 42
+
+  // open System;;
+  // Environment.CurrentDirectory;;
+  // Environment.CurrentDirectory <- @"c:\temp";;
+  //
+  // Environment.GetEnvironmentVariable("PATH")
+  //
+  // open System.IO;;
+  // Directory.GetFiles(".");;
+  //
+  // File.ReadLines("x.y") |> seq.iter ... // iter 
+  //
+  // File.ReadAllText(fname, [encoding])
+  //
+  // File.Exists("x.y")
+  // Directory.Exists("x.y")
+  //
+  let rec getAllFiles dir pattern =
+    seq { yield! Directory.EnumerateFiles(dir, pattern)
+          for d in Directory.EnumerateDirectories(dir) do
+              yield! getAllFiles d pattern }
+
+
+
+// Prints the methods, properties, and fields of a type to the console
+open System.Reflection
+let describeType (ty : Type) =
+     
+    let bindingFlags = 
+        BindingFlags.Public   ||| BindingFlags.NonPublic |||
+        BindingFlags.Instance ||| BindingFlags.Static    |||
+        BindingFlags.DeclaredOnly
+         
+    let methods = 
+        ty.GetMethods(bindingFlags) 
+        |> Array.fold (fun desc meth -> desc + sprintf "%s\r\n" meth.Name) ""
+            
+    let props = 
+        ty.GetProperties(bindingFlags)
+        |> Array.fold (fun desc prop -> desc + sprintf "%s\r\n" prop.Name) ""
+     
+    let fields =
+        ty.GetFields(bindingFlags)
+        |> Array.fold (fun desc field -> desc + sprintf "%s\r\n" field.Name) ""
+ 
+    printfn "Name: %s" ty.Name
+    printfn "Methods:    \n\t%s\n" methods
+    printfn "Properties: \n\t%s\n" props
+    printfn "Fields:     \n\t%s" fields
--- src/Graphs/Script.fsx
+++ src/Graphs/Script.fsx
@@ -4,5 +4,25 @@
 #load "Library.fs"
 open Graphs
 
-let num = Library.hello 42
+let num = Graphs.readGraph 42
 printfn "%i" num
+
+(*
+4 5
+2 1
+4 3
+1 4
+2 4
+3 2
+*)
+
+open System;; 
+open System.IO;; 
+
+let fdir = @"C:\Users\Enerqi\dev\fsharp\Graphs\src";;
+let fname = "undirected_graph.txt";;
+let f = Path.Combine(fdir, fname);;
+let dataLines = File.ReadLines(f);;
+// filter "empty" lines
+
+let header, edges = dataLines.[0], dataLines.[1..];;
