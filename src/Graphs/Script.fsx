99393ab37d05796b1aff4f8e049ebcb2ef0e39b4
enerqi <enerqi@nodomain.is>
2016-09-21 16:30:30 +0100

    Strongly connected components complete.

--- src/Graphs/Script.fsx
+++ src/Graphs/Script.fsx
@@ -13,27 +13,7 @@ open System.IO
 open Graphs
 
 
-let home = Environment.GetEnvironmentVariable("HOME")
-let maze_graph_file = Path.Combine(home, "dev/rust/mazes/le-graph.text")
-let test_graph_file file_name = 
-    let dir = Path.Combine(__SOURCE_DIRECTORY__, @"../../tests/Graphs.Tests/") 
-    Path.Combine(dir, file_name) |> Path.GetFullPath     
-let dag_file = test_graph_file "directed_graph.txt"
-let undirected_file = test_graph_file "undirected_graph.txt"
-let g_dag = Generation.readGraph dag_file true
-let g_undir = Generation.readGraph undirected_file false
-let g_maze = Generation.readGraph maze_graph_file false
-
-Algorithms.reverseDirectedGraph g_dag
-Algorithms.isDAG g_dag
-Algorithms.dfsPrePostOrderNumbers g_dag
-Algorithms.edgesSet g_dag
-
-Visualisation.toDotGraphDescriptionLanguage g_undir
-let outdir = __SOURCE_DIRECTORY__
-let outFile = Path.Combine(outdir, "dag")
-let dag_dot_definition = Visualisation.toDotGraphDescriptionLanguage g_dag
-let vizFile = Visualisation.makeGraphVisualisation dag_dot_definition outFile
+let outdir = __SOURCE_DIRECTORY__ // Places for generated graphs on the file system
 
 /// Use the system shell (command prompt) to open the file - F# interactive only
 let shellOpenFileWithDefaultApplication fileName =
@@ -46,6 +26,44 @@ let shellOpenFileWithDefaultApplication fileName =
     // it's really only the start up cost of cmd.exe that we are blocked on.
     Fake.ProcessHelper.ExecProcess setupProcess (TimeSpan.FromSeconds 2.0) 
 
-match vizFile with
-| Some(f) -> shellOpenFileWithDefaultApplication f |> ignore
-| _ -> ()
+let makeShowGraphViz (graph: Graph) vizName = 
+    let tryOpenVizFile fileName = 
+        match fileName with
+        | Some(f) -> shellOpenFileWithDefaultApplication f |> ignore
+        | _ -> ()
+    Visualisation.toDotGraphDescriptionLanguage graph
+    |> Visualisation.makeGraphVisualisation <| Path.Combine(outdir, vizName)
+    |> tryOpenVizFile
+
+let home = Environment.GetEnvironmentVariable("HOME")
+let test_graph_file file_name = 
+    let dir = Path.Combine(__SOURCE_DIRECTORY__, @"../../tests/Graphs.Tests/") 
+    Path.Combine(dir, file_name) |> Path.GetFullPath
+
+let dag_file = test_graph_file "directed_graph.txt"
+let g_dag = Generation.readGraph dag_file true
+
+let undirected_file = test_graph_file "undirected_graph.txt"
+let g_undir = Generation.readGraph undirected_file false
+
+let maze_graph_file = Path.Combine(home, "dev/rust/mazes/le-graph.text")
+let g_maze = Generation.readGraph maze_graph_file false
+
+let ssc_file = test_graph_file "strong_components_graph.txt"
+let g_scc = Generation.readGraph ssc_file true
+
+Algorithms.reverseDirectedGraph g_dag
+Algorithms.isDAG g_dag
+Algorithms.dfsPrePostOrderNumbers g_dag
+Algorithms.edgesSet g_dag
+Algorithms.stronglyConnectedComponents g_dag
+
+Visualisation.toDotGraphDescriptionLanguage g_undir
+
+let rev_scc = Algorithms.reverseDirectedGraph g_scc |> Option.get
+let s_comps = Algorithms.stronglyConnectedComponents g_scc |> Option.get
+
+
+makeShowGraphViz rev_scc "reverse_strong_components"
+makeShowGraphViz g_scc "strong_components"
+makeShowGraphViz g_dag "dag"
