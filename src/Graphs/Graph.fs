b3774ed6c433c64b89ed249957665fcc8ec6b27b
enerqi <enerqi@enerqi>
2016-10-12 12:37:50 +0100

    Add Graph.fs ADT.

---
+++ src/Graphs/Graph.fs
@@ -0,0 +1,13 @@
+﻿namespace Graphs
+
+module Graph = 
+
+    let vertexFromId graph (v: VertexId) =
+        if v.Id > 0 then 
+            graph.Vertices.[v.Id]
+        else 
+            failwith "<= zero is an invalid vertex id"
+
+    let verticesSeq graph = 
+        Seq.ofArray graph.Vertices |> Seq.skip 1
+
