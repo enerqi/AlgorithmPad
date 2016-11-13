537d9d56c381474a9e2ef99735f706aed3d26239
enerqi <enerqi@enerqi>
2016-11-13 00:43:25 +0000

    Use 'trial' (Result) computation expression in readGraph.

--- src/Graphs/Generation.fs
+++ src/Graphs/Generation.fs
@@ -30,54 +30,32 @@ module Generation =
     let readGraph isDirected (dataLines: seq<string>) : GraphResult<Graph> = 
         
         let header, edges = Seq.head dataLines, Seq.tail dataLines
-        
-        // Mutable datastructures + functional style Result type error handling is a 
-        // little painful/weird to code mixing pipelining with mutation.
-        // We use Result.map repeatedly to collect all the data needed to create the graph
-        // We could create multiple Results - one for vertex/edge counts, one for edges etc.
-        // but we would have to collect the multiple Results into one and something like 
-        // Result.collect is not definitely in the F# 4.1 API at the moment.
-        // It looks prettier pushing data through one pipeline aswell
 
-        let withVertexArray (verticesCount, edgesCount) : (int * int * Vertex []) = 
+        trial {
             // The entry at index 0 will be ignored. Keeping it saves on offset calculations.
             // 1-based indices. F# inclusive [x..y]
+            let! (verticesCount, edgesCount) = extractHeader header
             let vertexArray = [|for vIndex in [0..verticesCount] do
                                 yield { Identifier = VertexId vIndex;
-                                        Neighbours = new ResizeArray<VertexId>() } |]
-            (verticesCount, edgesCount, vertexArray)
-
-        let withEdgeVertexPairs (verticesCount, edgesCount, vertexArray) : (int * int * Vertex [] * seq<GraphResult<VertexId * VertexId>>) = 
+                                        Neighbours = new ResizeArray<VertexId>() } |]                
             let pairs = edges |> Seq.map extractVertexIdPair
-            (verticesCount, edgesCount, vertexArray, pairs)        
-            
-        let addAllEdgesToVertexArray ((_, _, vertexArray, pairs: seq<GraphResult<VertexId * VertexId>>), _) = 
-
             let addEdge (verticesArray: Vertex[]) (v1: VertexId) (v2: VertexId) = 
                 verticesArray.[v1.Id].Neighbours.Add(v2)
                 // v2 -> v1 for bi-directionality
                 if not isDirected then 
-                    verticesArray.[v2.Id].Neighbours.Add(v1)           
-                    
+                    verticesArray.[v2.Id].Neighbours.Add(v1) 
+            
             for pairResult in pairs do
-                 match pairResult with 
-                 | Ok ((v1, v2), _) -> addEdge vertexArray v1 v2                                
-                 | Bad e -> printfn "Ignoring vertex pair that could not parse: %A" e        
-                        
-           
-        let toGraph (verticesCount, edgesCount, vertexArray, _) = 
-            { VerticesCount = verticesCount; 
-              IsDirected = isDirected;
-              EdgesCount = edgesCount;
-              Vertices = vertexArray }              
+                match pairResult with 
+                | Ok ((v1, v2), _) -> addEdge vertexArray v1 v2                                
+                | Bad e -> printfn "Ignoring vertex pair that could not parse: %A" e 
 
-        
-        header
-        |> extractHeader        
-        |> lift withVertexArray
-        |> lift withEdgeVertexPairs
-        |> successTee addAllEdgesToVertexArray // side effecting
-        |> lift toGraph
+            return 
+                { VerticesCount = verticesCount; 
+                  IsDirected = isDirected;
+                  EdgesCount = edgesCount;
+                  Vertices = vertexArray }
+        }    
 
 
     /// Parse a line-oriented string representation from a file to create a graph
