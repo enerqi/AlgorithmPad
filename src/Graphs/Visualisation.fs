fc4de7b190ce58e19b89f43c53cad9123a80915d
enerqi <enerqi@enerqi>
2016-11-13 12:26:31 +0000

    trial computation expression for makeGraphVisualisation function.

--- src/Graphs/Visualisation.fs
+++ src/Graphs/Visualisation.fs
@@ -43,73 +43,36 @@ module Visualisation =
     /// Run the external 'dot' process to generate a graph image file
     /// Returns a Result of the image path or Error if creation failed
     let makeGraphVisualisation dotDescription outFilePathNoExtension = 
-        
-        // IO string (dir name)
-        let getOutFileDirectory = fun _ ->
-            Path.GetDirectoryName(outFilePathNoExtension)
-        
-        // We return a result and use >>=
-        // or return a value | throw the exception and use tryF around it
-        // or return a value an just use lift
-        // How to thread/append the new state through without stacking up ugly tuples?
-        // It might be nice to create a command and not do a side effect until done all pure work
-        // ...
-        // tryF is also meant to be a unit function so need to partial apply the result/state?
-        // or rather it is another successTee function? Tee will not change the result (but can do any side effect)
-        //  
-        // interesting that haskell do syntax sugar essentially nests bind like functions so a later function in 
-        // the chain can ref the state in the outer function, it does not need threading through as an additional
-        // parameter
-        // this is perhaps where we should look at the `trial` computation expression! Instead of these 
-        // ever growing tuples of local state.
 
-        // dirName -> Result IO string or Result IO () - unit meaning all went well
+        let getOutFileDirectory = 
+            tryF (fun _ -> Path.GetDirectoryName(outFilePathNoExtension)) FileAccessFailure
+        
         let checkDirExists outDir = 
             if not (Directory.Exists outDir) then 
                 let exn = new ArgumentException("outFilePathNoExtension", 
                                                 sprintf "Directory of out file (%s) does not exist" outDir)
                 fail (FileAccessFailure exn)
             else
-                ok outDir
+                ok ()
 
-        // could go first as does not need any input state, but it should not be done really until needed
-        let makeTempFile =
-            Path.GetTempFileName()
+        let makeTempFile = 
+            tryF (fun _ -> Path.GetTempFileName()) FileAccessFailure
+        
+        trial {
+            let! outDir = getOutFileDirectory
+            do! checkDirExists outDir
 
-        // templfile+dirName+[description] -> IO string (temp file name)
-        let writeDotTempFile = fun _ ->
-            let dotTempFileName = Path.GetTempFileName()
-            File.WriteAllText(dotTempFileName, dotDescription)
-            ok (outDir, dotTempFileName)
+            let! dotTempFileName = makeTempFile
+            
+            let writeDotTempFile = tryF (fun _ -> File.WriteAllText(dotTempFileName, dotDescription)) FileAccessFailure
+            do! writeDotTempFile
 
-        // tempFileName -> string * string * string
-        let processSetup dotTempFileName = 
             let dotCmd = "dot"
             let dotVisualisationFile = outFilePathNoExtension + ".png"
             let dotArgs = "-Tpng " + dotTempFileName + " -o " + dotVisualisationFile
-            (dotCmd, dotVisualisationFile, dotArgs)
-          
-        let runProcess = 
-            try
-                Proc.Shell.Exec(dotCmd, dotArgs, outDir) |> ignore
-                Some(dotVisualisationFile)
-            with 
-                | :? System.ComponentModel.Win32Exception as e ->
-                        printfn """Failed to run graph visual creation process "%s".\n%A""" (dotCmd + " " + dotArgs) e
-                        None                          
 
-        tryF getOutFileDirectory FileAccessFailure
-        >>= checkDirExists
-        |> tryF writeDotTempFile FileAccessFailure
-        
-        
-        //let dotTempFileName = Path.GetTempFileName()
-        //File.WriteAllText(dotTempFileName, dotDescription)
-        
-        //let dotCmd = "dot"
-        //let dotVisualisationFile = outFilePathNoExtension + ".png"
-        //let dotArgs = "-Tpng " + dotTempFileName + " -o " + dotVisualisationFile
-
-        
-
-
+            let runExternalProcess = tryF (fun _ -> Proc.Shell.Exec(dotCmd, dotArgs, outDir) |> ignore) VisualisationFailure
+            do! runExternalProcess
+       
+            return dotVisualisationFile
+        }
