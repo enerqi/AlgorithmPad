eeef5904efbbf5c995dd7b3f67beb4d912963310
enerqi <enerqi@enerqi>
2016-11-13 23:40:46 +0000

    More trial computation expression usage.

--- src/Graphs/Algorithms.fs
+++ src/Graphs/Algorithms.fs
@@ -15,6 +15,7 @@ module Algorithms =
         member this.Insert(vertexId: VertexId) = flags.Set(int32 vertexId.Id, true)
 
             
+    /// Return whether a path exists between two vertices on a graph
     let pathExists graph (v1: VertexId) (v2: VertexId) : GraphResult<bool> = 
         let visitedSet = VisitedSet(graph)
                 
@@ -24,119 +25,142 @@ module Algorithms =
                 let! vertex = vertexFromId graph v            
                 for neighbour in vertex.Neighbours do 
                     if not (visitedSet.Contains(neighbour)) then 
-                        explore neighbour // not tail recursive!
-                
-                return ()
+                        do! explore neighbour // not tail recursive!
             } 
         
-        explore v1
-        visitedSet.Contains(v2)
-            
-    let connectedComponents graph =              
+        trial {
+            do! explore v1
+            return visitedSet.Contains(v2)
+        }
+                    
+    /// Return the connected components of graph.
+    let connectedComponents graph: GraphResult<ResizeArray<ResizeArray<VertexId>>> =              
         let visitedSet = VisitedSet(graph)
         let mutable componentId = 0
         let componentGroups = new ResizeArray<ResizeArray<VertexId>>()
         
-        let rec explore (v: VertexId) = 
-            visitedSet.Insert(v) |> ignore
+        let rec explore (v: VertexId): GraphResult<unit> = 
+            visitedSet.Insert(v)
             componentGroups.[componentId].Add(v)
-            let vertex = vertexFromId graph v
-            for neighbour in vertex.Neighbours do
-                if not (visitedSet.Contains(neighbour)) then 
-                    explore neighbour 
+            trial {
+                let! vertex = vertexFromId graph v    
+                for neighbour in vertex.Neighbours do
+                    if not (visitedSet.Contains(neighbour)) then 
+                        do! explore neighbour 
+            }
 
-        for v in verticesSeq graph do 
-            if not (visitedSet.Contains(v.Identifier)) then
-                componentGroups.Add(new ResizeArray<VertexId>())
-                explore v.Identifier
-                componentId <- componentId + 1
+        trial {
+            for v in verticesSeq graph do 
+                if not (visitedSet.Contains(v.Identifier)) then
+                    componentGroups.Add(new ResizeArray<VertexId>())
+                    do! explore v.Identifier
+                    componentId <- componentId + 1
+            
+            return componentGroups
+        }
 
-        componentGroups
-
+    /// Return a new graph with all edges reversed. All edges point from their edge destinations to their sources.
+    /// An undirected graph is returned unchanged.
     let reverseDirectedGraph graph = 
         if graph.IsDirected then
             let reverseGraph = {graph with Vertices = 
                                            [|for v in graph.Vertices do
                                              yield {Identifier = v.Identifier; 
-                                                    Neighbours = new ResizeArray<VertexId>()}|]}
-            
+                                                    Neighbours = new ResizeArray<VertexId>()}|]}            
             for vertex in verticesSeq graph do                                   
                 for neighbourVertexId in vertex.Neighbours do 
                     if neighbourVertexId.Id < graph.VerticesCount then // In case the graph has some invalid entries
                         reverseGraph.Vertices.[neighbourVertexId.Id].Neighbours.Add(vertex.Identifier)
             
-            Some(reverseGraph)
+            reverseGraph
         else
-            None
+            graph
           
-
-    let isDAG graph = 
-        // Is it a directed *acyclic* graph?
-        // Option 1:
-        // - Check if the graph has any strongly connected components with more than vertex
-        //   This is best for finding all cycles in a graph, maybe overkill for finding if any cycles exist
-        // Option 2:
-        // - Detect any back edges in the DFS stack.
+    /// Is a graph directed and acyclic
+    let isDAG graph: GraphResult<bool> = 
+        
+        // - Detect any back edges in the Depth First Search stack.
         //   There is a cycle in a graph only if there is a back edge present in the graph (self link or link to parent). 
         //   This involves checking whether a new node exists in the current dfs stack.
-        
+        // Note, another option would be to check if there are any strongly connected components with more than one vertex        
         let checkDAG graph =
-            let visitedSet = VisitedSet(graph) // or make a BitArray
+            let visitedSet = VisitedSet(graph) 
             let dfsRecursionStackVertexIds: bool[] = Array.create (graph.VerticesCount + 1) false
             
-            let rec explore (vertexId: VertexId) =  // returns true if a back-edge is found                
-                dfsRecursionStackVertexIds.[vertexId.Id] || visitThisThenExploreChildren vertexId
+            let rec explore (vertexId: VertexId): GraphResult<bool> =  
+                if dfsRecursionStackVertexIds.[vertexId.Id] then 
+                    ok true
+                else 
+                    visitThisThenExploreChildren vertexId
+                
+            and visitThisThenExploreChildren (vertexId: VertexId): GraphResult<bool> = 
+                visitedSet.Insert(vertexId)
+                dfsRecursionStackVertexIds.[vertexId.Id] <- true // this vertex is on the stack
+                let mutable foundBackEdge = false 
 
-            and visitThisThenExploreChildren (vertexId: VertexId) = 
-                visitedSet.Insert(vertexId) |> ignore                
-                dfsRecursionStackVertexIds.[vertexId.Id] <- true
-
-                let vertex = vertexFromId graph vertexId                    
-                let mutable foundBackEdge = false // or use computation expressions http://tomasp.net/blog/imperative-i-return.aspx/
-                for neighbourVertexId in vertex.Neighbours do
-                    if not foundBackEdge && not (visitedSet.Contains(neighbourVertexId)) then 
-                        foundBackEdge <- explore vertexId
-                                                
-                dfsRecursionStackVertexIds.[vertexId.Id] <- false                    
-                foundBackEdge
-
-            let mutable foundBackEdge = false
-            for vertex in verticesSeq graph do
-                if not foundBackEdge && not (visitedSet.Contains(vertex.Identifier)) then
-                    foundBackEdge <- explore vertex.Identifier
+                trial {
+                    let! vertex = vertexFromId graph vertexId
+                    
+                    for neighbourVertexId in vertex.Neighbours do
+                        if not foundBackEdge && not (visitedSet.Contains(neighbourVertexId)) then 
+                            let! backEdgeExists = explore vertexId
+                            foundBackEdge <- backEdgeExists
+                            
+                    dfsRecursionStackVertexIds.[vertexId.Id] <- false // this vertex has finished being on the stack                    
+                    return foundBackEdge
+                }
             
-            foundBackEdge
+            trial {
+                let mutable foundBackEdge = false
+                for vertex in verticesSeq graph do
+                    if not foundBackEdge && not (visitedSet.Contains(vertex.Identifier)) then
+                        let! backEdgeExists = explore vertex.Identifier
+                        foundBackEdge <- backEdgeExists
 
-        graph.IsDirected && checkDAG graph
+                return foundBackEdge
+            }
 
-    let dfsPrePostOrderNumbers graph = 
+        if graph.IsDirected then
+            checkDAG graph
+        else
+            ok false
+
+    /// Return an array that can be indexed by VertexId.Id where each element contains the depth first search
+    /// pre and post order numbers. 0 is the first visited vertex. With each new node visited the number is incremented.
+    let dfsPrePostOrderNumbers graph: GraphResult<(int * int)[]> = 
         let visitedSet = VisitedSet(graph)
         let visitOrderNumbers = Array.create (graph.VerticesCount + 1) (0, 0)
         let mutable visitNumber = 0
 
-        let rec explore (vertexId: VertexId) = 
+        let rec explore (vertexId: VertexId): GraphResult<unit> = 
             
             let preOrderVisitNumber = visitNumber
-            visitedSet.Insert(vertexId) |> ignore
+            visitedSet.Insert(vertexId)
             visitOrderNumbers.[vertexId.Id] <- (preOrderVisitNumber, 0)
             visitNumber <- visitNumber + 1
 
-            let vertex = vertexFromId graph vertexId
-            for neighbourVertexId in vertex.Neighbours do 
-                if not (visitedSet.Contains(neighbourVertexId)) then 
-                    explore neighbourVertexId
+            trial {
+                let! vertex = vertexFromId graph vertexId
+                for neighbourVertexId in vertex.Neighbours do 
+                    if not (visitedSet.Contains(neighbourVertexId)) then 
+                        do! explore neighbourVertexId
 
-            let postOrderVisitNumber = visitNumber
-            visitOrderNumbers.[vertexId.Id] <- (preOrderVisitNumber, postOrderVisitNumber)
-            visitNumber <- visitNumber + 1
+                let postOrderVisitNumber = visitNumber
+                visitOrderNumbers.[vertexId.Id] <- (preOrderVisitNumber, postOrderVisitNumber)
+                visitNumber <- visitNumber + 1    
+            }
 
-        for v in verticesSeq graph do
-            if not (visitedSet.Contains(v.Identifier)) then
-                explore v.Identifier 
+        trial {
+            for v in verticesSeq graph do
+                if not (visitedSet.Contains(v.Identifier)) then
+                    do! explore v.Identifier 
 
-        visitOrderNumbers
-    
-    let stronglyConnectedComponents (graph: Graph) : ResizeArray<ResizeArray<VertexId>> option = 
+            return visitOrderNumbers
+        }
+        
+    /// Return the strongly connected components of the graph. 
+    /// Components are strongly connected when there is a cycle amongst them meaning that they can all reach each other somehow.
+    let stronglyConnectedComponents (graph: Graph) : GraphResult<ResizeArray<ResizeArray<VertexId>> option> = 
                 
         let findComponents reverseGraph = 
             // The vertex with the largest dfs post order number is in a source component
@@ -174,24 +198,37 @@ module Algorithms =
 
         in reverseDirectedGraph graph |> Option.map findComponents
 
-    let topologicalOrdering dag = 
-        // Read off the reverse post order numbers        
-        dfsPrePostOrderNumbers dag
-        |> Stream.ofArray
-        |> Stream.mapi (fun index prePostOrderNums -> (VertexId index, prePostOrderNums))
-        |> Stream.skip 1 // ignore the 0 index vertex put in to make 1 based indexing easier                                                           
-        |> Stream.sortBy (fun (_, (_, post)) -> -post) // Negative/reverse post 
-        |> Stream.map (fun (vId, _) -> vId)
-        |> Stream.toArray
+    
+    /// Return the topological ordering (source vertices before sink vertices) of a directed acyclic graph
+    let topologicalOrdering dag: GraphResult<VertexId []> = 
+
+        trial {
+            // Read off the reverse post order numbers 
+            let! dfsOrderings = dfsPrePostOrderNumbers dag
+            let orderedVertexIds = 
+                dfsOrderings
+                |> Stream.ofArray
+                |> Stream.mapi (fun index prePostOrderNums -> (VertexId index, prePostOrderNums))
+                |> Stream.skip 1 // ignore the 0 index vertex put in to make 1 based indexing easier                                                           
+                |> Stream.sortBy (fun (_, (_, post)) -> -post) // Negative/reverse post 
+                |> Stream.map (fun (vId, _) -> vId)
+                |> Stream.toArray
+            return orderedVertexIds
+        }
             
 
+    /// Return a set of all the edge (source, destination) vertex id pairs
     let edgesSet graph : Set<int * int> = 
-                    
+            
+        /// The function that given a source vertex index and a destination vertex id returns
+        /// (source index, destination index) pair        
         let edgeFrom : (int -> VertexId -> int * int) = 
             if graph.IsDirected then
                 (fun (vertexIndex: int) (neighbourVertexId: VertexId) ->
                     (vertexIndex, neighbourVertexId.Id))                    
             else
+                // For undirected graphs we need to order the pairs so that the set
+                // can remove duplicates - (1, 2) is really the same as (2, 1) when undirected
                 let orderEdgeVertices (a, b) = 
                     if a <= b then (a, b) else (b, a)                
                 (fun (vertexIndex: int) (neighbourVertexId: VertexId) ->
@@ -237,7 +274,8 @@ module Algorithms =
          ShortestPathDistances = distances
          ShortestPathTree = pathTree}
      
-
+    
+    /// Return the shortest path from a source vertex defined by the bfsData to a destination vertex
     let shortestPath (bfsData: BFS) (v: VertexId): ResizeArray<VertexId> option = 
 
         if bfsData.ShortestPathTree.[v.Id].IsSome then
--- src/Graphs/DomainTypes.fs
+++ src/Graphs/DomainTypes.fs
@@ -6,19 +6,6 @@ module DomainTypes =
     open Chessie.ErrorHandling
     open System
 
-    /// Type safe wrapper of vertex integer id
-    type VertexId = 
-        struct
-            val Id: int
-            new(id: int) = {Id = id}
-        end
-
-    /// Vertex (Node) in a graph using an adjacency list representation
-    type Vertex = {    
-        Identifier: VertexId
-        Neighbours: ResizeArray<VertexId>
-    }  
-
     /// Main Graph datastructure type implemented with mutable Vertex structures
     type Graph = {
         Vertices: Vertex array 
@@ -26,13 +13,17 @@ module DomainTypes =
         VerticesCount: int
         EdgesCount: int
     }
-
-    /// Type safe wrapper of distance integer values
-    type Distance = 
-        struct 
-            val Distance: uint32
-            new(d: uint32) = {Distance = d}
-        end
+    /// Vertex (Node) in a graph using an adjacency list representation
+    and Vertex = {    
+        Identifier: VertexId
+        Neighbours: ResizeArray<VertexId>
+    } 
+    /// Type safe wrapper of vertex integer id
+    and VertexId = 
+        struct
+            val Id: int
+            new(id: int) = {Id = id}
+        end            
 
     /// Breadth first search results from a source vertex
     type BFS = {
@@ -40,15 +31,20 @@ module DomainTypes =
         ShortestPathDistances: Distance option []
         ShortestPathTree: VertexId option []
     }
-    
-    type GraphAccessFailure = 
-        | InvalidVertexId of VertexId
+    /// Type safe wrapper of distance integer values
+    and Distance = 
+        struct 
+            val Distance: uint32
+            new(d: uint32) = {Distance = d}
+        end    
 
     type GraphFailure =
         | GraphAccessFailure of GraphAccessFailure
         | FileAccessFailure of Exception
         | ParsingFailure of string
         | VisualisationFailure of Exception
+    and GraphAccessFailure = 
+        | InvalidVertexId of VertexId
 
     /// Main functional style error handling result type used throughout the Graphs namespace
     type GraphResult<'TSuccess> = Result<'TSuccess, GraphFailure>  
