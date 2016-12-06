b35678dbebb8aa11317c402b594fa8e88c853e06
enerqi <enerqi@nodomain.is>
2016-12-06 17:36:17 +0000

    Generator skeleton for disjoint set tests.

--- tests/Graphs.Tests/DisjointSetTests.fs
+++ tests/Graphs.Tests/DisjointSetTests.fs
@@ -8,5 +8,34 @@ open FsUnit
 
 open Graphs
 
+module DisjointSetTestUtils =
+
+    let genSet : Gen<DisjointSet> = 
+        gen {
+            let! size = Arb.generate<DisjointSetSize>
+
+            let! unions = Gen.listOf Arb.generate<EntryId * EntryId>
+
+            if List.length unions > 0 then 
+                return DisjointSet.ofSeq size (Seq.ofList unions)
+            else 
+                return DisjointSet.make size
+        }
+
+    type DisjointSetGenerators = 
+        static member DisjointSet() = 
+            { new Arbitrary<DisjointSet>() with
+                  override this.Generator = genSet 
+                  override this.Shrinker t = Seq.empty }
+
+
+open DisjointSetTestUtils
+
 [<Tests>]
-let disjointSetTests = []
+let disjointSetTests = 
+    Arb.register<DisjointSetGenerators>() |> ignore
+
+    testList "DisjointSet ADT" [
+
+    ]
+    
